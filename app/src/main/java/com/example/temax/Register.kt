31c70ab37b5bae848b63a235e859a0a41913fdef
renators77 <a23511@alunos.ipca.pt>
2023-10-11 11:25:13 +0100

    Insert a on click in the button, test

--- app/src/main/java/com/example/temax/Register.kt
+++ app/src/main/java/com/example/temax/Register.kt
@@ -2,10 +2,14 @@ package com.example.temax
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
     }
+
+    fun Com_Jeitinho(view: View) {}
+
 }
