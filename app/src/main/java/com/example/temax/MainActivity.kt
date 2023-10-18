fc19f73fc2fe8dde0e7e283b1252f5d2a260cdd7
pinante <Mario.joao31@hotmail.com>
2023-10-18 18:38:53 +0100

    Criação do registar com as funcoes para retornar dados para o login

--- app/src/main/java/com/example/temax/MainActivity.kt
+++ app/src/main/java/com/example/temax/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.temax
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val emailEditText: EditText by lazy { findViewById(R.id.main_editText) }
+    val passwordEditText: EditText by lazy { findViewById(R.id.main_editText2)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Check if the result is OK and handle the received data
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            // Now you can use resultData as needed
+            val emailReceived = data?.getStringExtra("email")
+
+            if (emailReceived != null) {
+                //usado para meter o texto na caixa de email se não for null
+                runOnUiThread {
+                    emailEditText.setText(emailReceived)
+                }
+            }
+        }
+    }
+
+    fun main_buttonLogin(view: View) {
+        Toast.makeText(this, emailEditText.text.toString(), Toast.LENGTH_SHORT).show()
+
+    }
+
+    fun main_buttonRegister(view: View){
+        val intent = Intent(this@MainActivity, Register::class.java)
+        startActivityForResult(intent,1)
+    }
+
+    fun validate(emailAdrees: String, password: String):Boolean{
+        //TODO: fazer aqui depois o request a API para ver se tem permissao
+
+        return true;
     }
 }
--- app/src/main/java/com/example/temax/Register.kt
+++ app/src/main/java/com/example/temax/Register.kt
@@ -1,15 +1,40 @@
 package com.example.temax
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
+import android.widget.EditText
 
 class Register : AppCompatActivity() {
+
+    val emailEditText: EditText by lazy { findViewById(R.id.editTextText) }
+    val passwordEditText: EditText by lazy { findViewById(R.id.editTextTextPassword)}
+    val dateEditText: EditText by lazy { findViewById(R.id.editTextDate)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
     }
 
-    fun Com_Jeitinho(view: View) {}
+    fun registar_button(view: View){
+        //TODO: por aqui o request a api para registar para depois mandar o user de volta para o login
+        if(verificationsRegister(emailEditText.text.toString(),
+                passwordEditText.text.toString(),
+                dateEditText.text.toString())) {
+                      
+        }
+    }
+    fun end(){
+        val returnIntent = Intent()
+        // não esquecer de por sempre o to string porque senão vai retornal o editavel em vez de string
+        //TODO: por aqui a variavel para passar para o login
+        //returnIntent.putExtra("email", xxxxxx)
+        setResult(Activity.RESULT_OK, returnIntent)
+    }
 
+    fun verificationsRegister(email: String, password: String, dateNasc: String ): Boolean{
+        return true
+    }
 }
