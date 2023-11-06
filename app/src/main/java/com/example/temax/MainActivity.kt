c8e63d1428db7772aa8a7fc58caed2d8c269cf18
pinante <Mario.joao31@hotmail.com>
2023-11-06 16:20:53 +0000

    login complete

--- app/src/main/java/com/example/temax/MainActivity.kt
+++ app/src/main/java/com/example/temax/MainActivity.kt
@@ -4,9 +4,16 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     val emailEditText: EditText by lazy { findViewById(R.id.main_editText) }
@@ -34,10 +41,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun main_buttonLogin(view: View) {
-        Toast.makeText(this, emailEditText.text.toString(), Toast.LENGTH_SHORT).show()
 
-        val intent = Intent(this@MainActivity, SelectTypeUser::class.java)
-        startActivityForResult(intent,1)
+        val r = Runnable {
+            validate(emailEditText.text.toString(),passwordEditText.text.toString())
+        }
+        val t = Thread(r)
+        t.start()
 
     }
 
@@ -46,9 +55,69 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(intent,1)
     }
 
-    fun validate(emailAdrees: String, password: String):Boolean{
+    fun validate(emailAdrees: String, password: String){
         //TODO: fazer aqui depois o request a API para ver se tem permissao
+        val client = OkHttpClient()
 
-        return true;
+
+        var jsonBody = """
+{
+    "Email": "${emailEditText.text}",
+    "Password": "${passwordEditText.text}"
+}
+"""
+
+        // Define the request body and media type (usually JSON)
+        val requestBody =
+            jsonBody.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+
+        val  requestBuilder = Request.Builder()
+            .url("http://192.168.1.74:3000/users/login")
+            .post(requestBody)
+
+        //headers que preciso para o Post
+        val headers = mapOf(
+            "Content-Type" to "application/json"
+        )
+
+        // Add headers if needed
+        headers?.forEach { (key, value) ->
+            requestBuilder.addHeader(key, value)
+        }
+
+        val request = requestBuilder.build()
+
+        try {
+            val response: Response = client.newCall(request).execute()
+            val responseBody = response.body?.string()
+
+            if (response.isSuccessful) {
+                // Handle the successful response
+                // Here, you can check the response body or perform any validation
+                val isValid = validateResponse(responseBody)
+                if(isValid){
+                    //se for o login existir passa para a activyti seguinte
+                    val intent = Intent(this@MainActivity, SelectTypeUser::class.java)
+                    startActivityForResult(intent,1)
+                }
+            } else {
+                // Handle the error response
+                Log.d("ErrorPost","deu merda no post");
+                // You can log the error or return false, indicating validation failure
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            // Handle network or other exceptions
+        }
+
     }
+}
+
+fun validateResponse(responseBody: String?): Boolean {
+    // Implement your response validation logic here
+    // Parse the response and check if it's valid
+    // For example, check if the response contains "Login successful" or other relevant data
+
+    // Replace the following line with your actual validation logic
+    return responseBody?.contains("Login exists") == true
 }
