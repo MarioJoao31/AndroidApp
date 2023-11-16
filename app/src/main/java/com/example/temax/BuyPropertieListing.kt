ab2346372aaf819ded6e6da2b9e4840b34fb8f2c
pinante <Mario.joao31@hotmail.com>
2023-11-16 14:26:55 +0000

    added class houses and list of houses to sell

--- app/src/main/java/com/example/temax/BuyPropertieListing.kt
+++ app/src/main/java/com/example/temax/BuyPropertieListing.kt
@@ -3,7 +3,8 @@ package com.example.temax
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
-import com.example.temax.adapters.ListViewSellAnnounceAdapter
+import com.example.temax.adapters.AdapterListViewBuyProperties
+import com.example.temax.classes.House
 
 class BuyPropertieListing : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +14,12 @@ class BuyPropertieListing : AppCompatActivity() {
         // Dentro da sua Activity ou Fragment
         val listView = findViewById<ListView>(R.id.listviewBuyProperties)
 
-        val adapter = ListViewSellAnnounceAdapter(this)
-        adapter.addSellAnnounce(R.mipmap.ic_buyhouse, "12345-678", "Linda casa com 3 quartos")
-        adapter.addSellAnnounce(R.mipmap.ic_rent, "54321-876", "Apartamento moderno no centro")
+        //dados para a lista
+        var house1 = House(1,2,123.0,2001, 2,"sim",3,"descricao,","4710-306",55,20,2,"Sell")
+        var house2 = House(1,2,123.0,2001, 2,"sim",3,"descricao,","4710-306",55,20,2,"Sell")
+
+        val values = mutableListOf<House>(house1,house2)
+        val adapter = AdapterListViewBuyProperties(this,R.layout.custum_listview_annonces,values)
 
         listView.adapter = adapter
     }
--- app/src/main/java/com/example/temax/adapters/AdapterListViewBuyProperties.kt
+++ app/src/main/java/com/example/temax/adapters/AdapterListViewBuyProperties.kt
@@ -1,14 +1,61 @@
 package com.example.temax.adapters
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.temax.R
+import com.example.temax.classes.House
 
-class AdapterListViewBuyProperties(context: Context) :
-    ArrayAdapter<AdapterListViewBuyProperties>(context, R.layout.custum_listview_annonces) {
+class AdapterListViewBuyProperties(context: Context,resource: Int, objects: MutableList<House> ) :
+    ArrayAdapter<House>(context, resource,objects) {
 
     private val inflater: LayoutInflater = LayoutInflater.from(context)
-    private val sellAnnounces = mutableListOf<ListViewSellAnnounceAdapter.SellAnnounce>()
+    private val listHouses = mutableListOf<House>()
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view: View
+        val vh: MyViewHolder
+
+        //serve para melhorar o processo de memoria e de guardar as list views que ja passaram
+        if(convertView != null){
+            view = convertView
+        }else{
+            view = LayoutInflater.from(context).inflate(R.layout.custum_listview_annonces,parent,false)
+            view.tag = MyViewHolder(view)
+        }
+
+        //serve para fazer referencia a class abaixo
+        vh = view.tag as MyViewHolder
+        //serve para ir buscar os valores da lista
+        val house = getItem(position)
+
+        //verificação se os valores não forem nullos
+        if(house != null){
+            if (house.ListingType == "sell"){
+                vh.imagem?.setBackgroundColor(Color.GREEN)
+            }else{
+                vh.imagem?.setBackgroundColor(Color.RED)
+            }
+            vh.codigo_postal?.text = house.Postal_code
+            vh.descricao?.text = house.Description
+        }
+
+        return view
+    }
+
+}
+
+//serve para guardar as views
+private class MyViewHolder(view: View?){
+    //image
+    val imagem = view?.findViewById<ImageView>(R.id.image_sell_announce)
+    //codigo postal
+    val codigo_postal = view?.findViewById<TextView>(R.id.text_postal_code)
+    //descricao
+    val descricao = view?.findViewById<TextView>(R.id.text_description)
 }
