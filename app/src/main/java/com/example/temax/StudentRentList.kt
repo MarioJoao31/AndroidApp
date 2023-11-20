c791f4f54572c7d8fd8fa209e6102777ec612e56
renators77 <a23511@alunos.ipca.pt>
2023-11-20 18:51:19 +0000

    AdapterListViewRentProperties, finalizado, conseguindo assim fazer a listagem de casas e apartamentos para arrendar. Juntamente fiz alterações no StudentRentList. Tudo operacional, falta adicionar os quartos.

--- app/src/main/java/com/example/temax/StudentRentList.kt
+++ app/src/main/java/com/example/temax/StudentRentList.kt
@@ -1,11 +1,10 @@
 package com.example.temax
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ListView
-import com.example.temax.adapters.AdapterListViewBuyProperties
+import androidx.appcompat.app.AppCompatActivity
+import com.example.temax.adapters.AdapterListViewRentProperties
 import com.example.temax.classes.Apartement
 import com.example.temax.classes.House
 import com.example.temax.services.ApartementServices
@@ -15,39 +14,89 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 class StudentRentList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_rent_list)
 
+        // Encontra a ListView no layout da activity
         val listView = findViewById<ListView>(R.id.listview_student_rent_announces)
 
-        //URL para a função de casas para arrendar /
-        val BASE_URL = "http://${BuildConfig.API_IP}:3000/house/rentHouses/"
+        // URL para a função de casas para arrendar
+        val houseBaseUrl = "http://${BuildConfig.API_IP}:3000/house/rentHouses/"
 
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
+        // Configuração do Retrofit para o houseService
+        val houseRetrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(houseBaseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        val service = retrofit.create(HouseServices::class.java)
+        val houseService = houseRetrofit.create(HouseServices::class.java)
 
-        //chamada do serviço getRentHouses proveniente do HouseServices
-        val call = service.getRentHouses()
-        call.enqueue(object : Callback<List<House>>{
-            override fun onResponse(call : Call<List<House>>, response: Response<List<House>>){
-                if(response.code() == 200){
-                    val respondeBody = response.body()
-                    val adapter = AdapterListViewBuyProperties(this@StudentRentList,R.layout.activity_student_rent_list,respondeBody.orEmpty())
-                    listView.adapter = adapter
-                    Log.d("resposta",respondeBody.toString())
+        // URL para a função de apartamentos para arrendar
+        val apartementBaseUrl = "http://${BuildConfig.API_IP}:3000/apartement/rentApartements/"
+
+        // Configuração do Retrofit para o apartementService
+        val apartementRetrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(apartementBaseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apartementService = apartementRetrofit.create(ApartementServices::class.java)
+
+        // Chamada do serviço getRentHouses proveniente do HouseServices
+        val callHouses = houseService.getRentHouses()
+
+        // Chamada do serviço getRentApartements proveniente do ApartementServices
+        val callApartements = apartementService.getRentApartements()
+
+        // Callback para a resposta do serviço getRentHouses()
+        callHouses.enqueue(object : Callback<List<House>> {
+            override fun onResponse(call: Call<List<House>>, response: Response<List<House>>) {
+                if (response.code() == 200) {
+                    val houseList = response.body()
+
+                    // Callback para a resposta do serviço getRentApartements
+                    callApartements.enqueue(object : Callback<List<Apartement>> {
+                        override fun onResponse(
+                            call: Call<List<Apartement>>,
+                            response: Response<List<Apartement>>
+                        ) {
+                            if (response.code() == 200) {
+                                val apartementList = response.body()
+
+                                // Combina as listas de casas e apartamentos
+                                val combinedList = mutableListOf<Any>()
+                                combinedList.addAll(houseList.orEmpty())
+                                combinedList.addAll(apartementList.orEmpty())
+
+                                // Configuração do AdapterListViewRentProperties com a lista combinada
+                                val adapter =
+                                    AdapterListViewRentProperties(
+                                        this@StudentRentList,
+                                        R.layout.activity_student_rent_list,
+                                        combinedList
+                                    )
+                                listView.adapter = adapter
+                            }
+                        }
+
+                        override fun onFailure(
+                            call: Call<List<Apartement>>,
+                            t: Throwable
+                        ) {
+                            // Log de erro caso a chamada do ApartementService falhe
+                            Log.e("StudentRentList", "Error fetching Apartements", t)
+                        }
+                    })
                 }
             }
 
             override fun onFailure(call: Call<List<House>>, t: Throwable) {
-                print("error")
+
+                // Log de erro caso a chamada do ApartementService falhe
+                Log.e("StudentRentList", "Error fetching Houses", t)
             }
         })
     }
--- app/src/main/java/com/example/temax/adapters/AdapterListViewRentProperties.kt
+++ app/src/main/java/com/example/temax/adapters/AdapterListViewRentProperties.kt
@@ -1,4 +1,80 @@
 package com.example.temax.adapters
 
-class AdapterListViewRentProperties {
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.temax.R
+import com.example.temax.classes.Apartement
+import com.example.temax.classes.House
+
+class AdapterListViewRentProperties(
+    context: Context,
+    resource: Int,
+    objects: List<Any>
+) : ArrayAdapter<Any>(context, resource, objects) {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view: View
+        val vh: MyViewHolder
+
+        //serve para melhorar o processo de memoria e de guardar as list views que ja passaram
+        if (convertView != null) {
+            view = convertView
+        } else {
+            // expand a view do layout  para cada item da lista
+            view = LayoutInflater.from(context).inflate(R.layout.custum_listview_annonces, parent, false)
+            view.tag = MyViewHolder(view)
+        }
+
+        //serve para fazer referencia a class abaixo
+        vh = view.tag as MyViewHolder
+
+        //serve para ir buscar os valores da lista
+        val property = getItem(position)
+
+        if (property is House) {
+            //  Logica  para House
+            if (property.ListingType.equals("rent", ignoreCase = true)) {
+                vh.imagem?.setBackgroundColor(Color.RED) // Cor diferente para casas para alugar
+            } else {
+                vh.imagem?.setBackgroundColor(Color.GREEN) // Cor padrão para casas
+            }
+            vh.price?.text = property.Price.toString()
+            vh.descricao?.text = property.Description
+            vh.elevator?.text = property.Elevator
+            vh.wcs?.text = property.WCs.toString()
+            vh.totalArea?.text = property.Total_lot_area.toString()
+
+        } else if (property is Apartement) {
+            //  Logica  para Apartement
+            if (property.ListingType.equals("rent", ignoreCase = true)) {
+                vh.imagem?.setBackgroundColor(Color.BLUE) // Cor diferente para apartamentos para alugar
+            } else {
+                vh.imagem?.setBackgroundColor(Color.GREEN) // Cor padrão para apartamentos
+            }
+            vh.price?.text = property.Price.toString()
+            vh.descricao?.text = property.Description
+            vh.elevator?.text = property.Elevator
+            vh.wcs?.text = property.WCs.toString()
+        }
+
+        return view
+    }
+
+    // Classe interna para armazenar as views
+    private class MyViewHolder(view: View?) {
+        val imagem = view?.findViewById<ImageView>(R.id.image_sell_announce)
+        val price = view?.findViewById<TextView>(R.id.text_price)
+        val wcs = view?.findViewById<TextView>(R.id.text_WCs)
+        val totalArea = view?.findViewById<TextView>(R.id.text_total_area)
+        val descricao = view?.findViewById<TextView>(R.id.text_description)
+        val elevator = view?.findViewById<TextView>(R.id.text_elevator)
+    }
 }
