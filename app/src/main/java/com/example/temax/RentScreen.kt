42a88ec440bf9e77b77630534589b8d0bda48ef9
Ruben <rubenfilipe2908@gmail.com>
2023-11-21 15:28:08 +0000

    Update Rent Screen

--- app/src/main/java/com/example/temax/RentScreen.kt
+++ app/src/main/java/com/example/temax/RentScreen.kt
@@ -3,10 +3,20 @@ package com.example.temax
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ListView
 import com.example.temax.adapters.AdapterListViewBuyProperties
+import com.example.temax.adapters.AdapterListViewRentProperties
 import com.example.temax.adapters.ListViewSellAnnounceAdapter
+import com.example.temax.classes.Apartement
 import com.example.temax.classes.House
+import com.example.temax.services.ApartementServices
+import com.example.temax.services.HouseServices
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RentScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,13 +26,82 @@ class RentScreen : AppCompatActivity() {
         // Dentro da sua Activity ou Fragment
         val listView = findViewById<ListView>(R.id.listview_rent_announces)
 
-        //dados para a lista
-        //var house1 = House(1,2,123.0,2001, 2,"sim",3,"descricao,","4710-306",55,20,2,"Rent")
+        // URL para a função de casas para arrendar
+        val houseBaseUrl = "http://${BuildConfig.API_IP}:3000/house/rentHouses/"
 
-        //val values = mutableListOf<House>(house1,house2)
-        //val adapter = AdapterListViewBuyProperties(this,R.layout.custum_listview_annonces,values)
+        // Configuração do Retrofit para o houseService
+        val houseRetrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(houseBaseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-        //listView.adapter = adapter
+        val houseService = houseRetrofit.create(HouseServices::class.java)
+
+        // URL para a função de apartamentos para arrendar
+        val apartementBaseUrl = "http://${BuildConfig.API_IP}:3000/apartement/rentApartements/"
+
+        // Configuração do Retrofit para o apartementService
+        val apartementRetrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(apartementBaseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apartementService = apartementRetrofit.create(ApartementServices::class.java)
+
+        // Chamada do serviço getRentHouses proveniente do HouseServices
+        val callHouses = houseService.getRentHouses()
+
+        // Chamada do serviço getRentApartements proveniente do ApartementServices
+        val callApartements = apartementService.getRentApartements()
+
+        // Callback para a resposta do serviço getRentHouses()
+        callHouses.enqueue(object : Callback<List<House>> {
+            override fun onResponse(call: Call<List<House>>, response: Response<List<House>>) {
+                if (response.code() == 200) {
+                    val houseList = response.body()
+
+                    // Callback para a resposta do serviço getRentApartements
+                    callApartements.enqueue(object : Callback<List<Apartement>> {
+                        override fun onResponse(
+                            call: Call<List<Apartement>>,
+                            response: Response<List<Apartement>>
+                        ) {
+                            if (response.code() == 200) {
+                                val apartementList = response.body()
+
+                                // Combina as listas de casas e apartamentos
+                                val combinedList = mutableListOf<Any>()
+                                combinedList.addAll(houseList.orEmpty())
+                                combinedList.addAll(apartementList.orEmpty())
+
+                                // Configuração do AdapterListViewRentProperties com a lista combinada
+                                val adapter =
+                                    AdapterListViewRentProperties(
+                                        this@RentScreen,
+                                        R.layout.activity_student_rent_list,
+                                        combinedList
+                                    )
+                                listView.adapter = adapter
+                            }
+                        }
+
+                        override fun onFailure(
+                            call: Call<List<Apartement>>,
+                            t: Throwable
+                        ) {
+                            // Log de erro caso a chamada do ApartementService falhe
+                            Log.e("StudentRentList", "Error fetching Apartements", t)
+                        }
+                    })
+                }
+            }
+
+            override fun onFailure(call: Call<List<House>>, t: Throwable) {
+
+                // Log de erro caso a chamada do ApartementService falhe
+                Log.e("StudentRentList", "Error fetching Houses", t)
+            }
+        })
 
         listView.setOnItemClickListener { _, _, position, _ ->
             // Iniciar a atividade SelectedHouse e passar os detalhes da casa selecionada através de Intent extras
