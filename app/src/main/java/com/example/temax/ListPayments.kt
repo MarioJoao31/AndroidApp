b16cd753f702865efd47639deff7a387bf55b247
Ruben <rubenfilipe2908@gmail.com>
2024-01-08 15:27:50 +0000

    Intent para ListagemPayments

---
+++ app/src/main/java/com/example/temax/ListPayments.kt
@@ -0,0 +1,11 @@
+package com.example.temax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListPayments : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_payments)
+    }
+}
